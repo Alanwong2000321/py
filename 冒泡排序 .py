65daec1dcd96916a90c251a8634ae8ed94c3e709
Alanwong2000321 <49506230+Alanwong2000321@users.noreply.github.com>
2019-10-17 16:43:08 +0800

    Create 冒泡排序 .py

---
+++ 冒泡排序 .py
@@ -0,0 +1,13 @@
+
+def bubble_sort(arr):
+    size = len(arr)
+    for i in range(size):
+        for j in range(size-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j+1], arr[j] = arr[j], arr[j+1]
+    return arr
+ 
+n = int(input())
+nums = [int(x) for x in input().split()]
+nums = bubble_sort(nums)
+print(*nums)
