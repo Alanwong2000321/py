7bff869067fa5a8571a8e6bd0dba13fc81e12742
Alanwong2000321 <49506230+Alanwong2000321@users.noreply.github.com>
2019-11-10 22:14:11 +0800

    Delete 幸运数字.py

--- 幸运数字.py
+++
@@ -1,9 +0,0 @@
-find = False
-m, n = [int(x) for x in input().split()]
-for i in range(n, m-1, -1):
-    if i % 7 == 0 and i % 4 != 0:
-        print(i)
-        find = True
-        break
-if not find:
-    print("no")
