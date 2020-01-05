c0a2824d872b601e50da6db96ac960af0881ceca
Alanwong2000321 <49506230+Alanwong2000321@users.noreply.github.com>
2020-01-05 23:31:29 +0800

    Revert "Create 选择排序.py"
    This reverts commit 79e034a2ccda6a60320521ecb840fb2c452a2680.

--- 算法/选择排序.py
+++
@@ -1,9 +0,0 @@
-A = [4, 2, 1, 290, 51]   
-for i in range(len(A)): 
-  min1 = i 
-  for j in range(i+1, len(A)): 
-    if A[min1] > A[j]:
-      min1 = j 
-      A[i], A[min1] = A[min1], A[i] 
-for i in range(len(A)): 
-    print("%d" %A[i]),
